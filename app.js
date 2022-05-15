9baa3755fc840e4afe1f0196da10e6f1c3ff7da1
Kent Lu <Kent.Lu@idtechproducts.com>
2022-05-15 21:36:07 +0800

    Add swagger document

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 
 let app = express();
+
+let swaggerDocument;
+
+const swaggerJsdoc = require('swagger-jsdoc');
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'KentLu Exam API',
+      version: 'v1',
+      description: 'KentLu Exam API document with swagger',
+    },
+  },
+  apis: ['./routes/*.js', './routes/*/*/*.js', './swagger.yaml'],
+};
+swaggerDocument = swaggerJsdoc(options);
 
 // CORS
 app.use(
@@ -16,6 +33,8 @@ app.use(
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   }),
 );
+
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 // Transform buffer body to specified type
 app.use(require('./middleware/contentTypeTransform').contentTypeTransformMiddleware);
--- routes/exam/v1/auth.js
+++ routes/exam/v1/auth.js
@@ -4,6 +4,31 @@ const { asyncMiddleware } = require('../../common');
 const AuthService = require('../../../services/auth');
 const _ = require('lodash');
 
+/**
+ * @swagger
+ * /exam/v1/auth/auth0:
+ *   post:
+ *     tags:
+ *       - auth
+ *     summary: "[Web] Verify Auth0 toekn"
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               idToken:
+ *                 type: string
+ *                 description: Auth0 token
+ *             example:
+ *               idToken: xxxxxxx
+ *             required:
+ *               - idToken
+ *     responses:
+ *       default:
+ *         $ref: "#/components/responses/Default"
+ */
 router.post(
   '/auth0',
   asyncMiddleware(async function (req) {
